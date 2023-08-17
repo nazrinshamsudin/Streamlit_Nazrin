3ecc405f70de02a3760e6705e819214df63695de
Nazrin Shamsudin <43793125+mnazrinshamsudin@users.noreply.github.com>
2023-08-17 15:17:43 +0800

    Update Alphaton.py

--- Alphaton.py
+++ Alphaton.py
@@ -27,8 +27,8 @@ selected_tickers = ["AAPL", "MSFT", "AMZN", "GOOGL", "NVDA"]
 selected_period = '1y'
 
 selected_tickers.append("SPY")
-spy_data = fetch_company_data("SPY", f"{selected_period}y")
-spy_data = fetch_company_data("SPY", selected_start_date.date())
+#spy_data = fetch_company_data("SPY", f"{selected_period}y")
+spy_data = fetch_company_data("SPY", f"{selected_start_date.date()}")
 
 
 

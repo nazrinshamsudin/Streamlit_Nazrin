ca880ca4b03f8ea9a4f8830f21296efdcad2a617
Nazrin Shamsudin <43793125+mnazrinshamsudin@users.noreply.github.com>
2023-08-21 13:44:12 +0800

    Update Alphaton.py

--- Alphaton.py
+++ Alphaton.py
@@ -208,7 +208,7 @@ if isinstance(selected_start_date, pd.Timestamp):
 #Adding a green and bigger dot for SPY as a benchmark
 scatter_fig.add_trace(go.Scatter(
     x=[1.0],  # SPY correlation is always 1
-    y=[scaled_covariance_matrix.loc["SPY", "SPY"]],  # SPY covariance with itself
+    y=[scaled_covariance_matrix.loc],  # SPY covariance with itself
     mode='markers',
     marker=dict(color="lightgreen", size=21),  # Dark green and bigger dot
     text=["SPY (Cov: Max, Corr: 1.00)"],

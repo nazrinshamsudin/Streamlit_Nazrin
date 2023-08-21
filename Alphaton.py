01899e4c3e8029109400a01a026240bde508bf89
Nazrin Shamsudin <43793125+mnazrinshamsudin@users.noreply.github.com>
2023-08-21 12:34:09 +0800

    Update Alphaton.py

--- Alphaton.py
+++ Alphaton.py
@@ -171,7 +171,7 @@ scatter_fig = go.Figure()
 
 
 # Adding scatter plot for selected companies
-for ticker, covariance, correlation in zip(cov_corr_df["Ticker"], sorted_cov_corr_df["Scaled Covariance"], cov_corr_df["Correlation with SPY"]):
+for ticker, scaled_covariance, correlation in zip(cov_corr_df["Ticker"], sorted_cov_corr_df["Scaled Covariance"], cov_corr_df["Correlation with SPY"]):
     scatter_fig.add_trace(go.Scatter(
         x=[correlation],
         y=[scaled_covariance],

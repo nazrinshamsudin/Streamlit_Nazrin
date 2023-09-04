dd98335dbb8942304f1c34cadbd2667fb0c5cdb4
Nazrin Shamsudin <43793125+mnazrinshamsudin@users.noreply.github.com>
2023-09-04 11:11:40 +0800

    Update Alphaton.py

--- Alphaton.py
+++ Alphaton.py
@@ -50,7 +50,7 @@ def fetch_company_data(tickers, period):
         return None
 
 # Load S&P 500 tickers
-sp500_table = wikipedia.page("List_of_S&P_500_component_stocks").html().encode("UTF-8")
+sp500_table = wikipedia.page("List_of_S&P_500_companies").html().encode("UTF-8")
 df = pd.read_html(sp500_table)[0]
 sp500_tickers = df["Symbol"].tolist()
 
